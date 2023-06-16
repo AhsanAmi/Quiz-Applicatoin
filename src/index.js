c177421f1086cd9de9b954f8b9435b7b519886ce
Ahsan Amin <106345768+AhsanAmi@users.noreply.github.com>
2023-06-16 23:09:00 +0500

    separate question array

--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
 import './main.css'
+import quizQuestions from './questionArray'
 let userAnswers = [];
 let currentQuestionIndex = 0;
 let score = 0;
-// Sample quiz questions
-const quizQuestions = [ {
-	question: "Q:1 What is the capital of France?"
-	, options: [ "Paris", "London", "Berlin", "Rome" ]
-	, answer: "Paris"
-}, {
-	question: "Q:2 Who painted the Mona Lisa?"
-	, options: [ "Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo" ]
-	, answer: "Leonardo da Vinci"
-}, {
-	question: "Q:3 What is the largest planet in our solar system?"
-	, options: [ "Jupiter", "Mars", "Earth", "Saturn" ]
-	, answer: "Jupiter"
-}, {
-	question: "Q:4 Which country won the FIFA World Cup in 2018?"
-	, options: [ "France", "Brazil", "Germany", "Spain" ]
-	, answer: "France"
-}, {
-	question: "Q:5 Who wrote the Harry Potter book series?"
-	, options: [ "J.K. Rowling", "Stephen King", "George R.R. Martin", "Dan Brown" ]
-	, answer: "J.K. Rowling"
-}, {
-	question: "Q:6 What is the chemical symbol for gold?"
-	, options: [ "Au", "Ag", "Fe", "Cu" ]
-	, answer: "Au"
-}, ];
+
 // DOM elements
 const questionContainer = document.getElementById( "question-container" );
 const questionElement = document.getElementById( "question" );
